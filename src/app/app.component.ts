9b1cb3d65a0b35eb971cd33ff7862779efc84244
= <celotts@gmail.com>
2021-03-13 20:41:05 -0300

    Se actualiza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form-reactive';
+  constructor(private formBuilder: FormBuilder) { }
+
+  formReactive = this.formBuilder.group({
+    nombre: [''],
+    apellido: ['', Validators.required],
+    dirección: ['', Validators.required],
+    direccion2: ['', Validators.required],
+    email: ['', Validators.compose([Validators.email, Validators.required])],
+    ciudad: ['', Validators.required],
+    estado: ['', Validators.required],
+    zip: ['', Validators.required],
+    checkMe: ['', Validators.required],
+  });
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.formReactive.controls.nombre.setValue('Carlos E. Lott');
+    this.formReactive.controls.apellido.setValue('Carlos E. Lott')
+  }
 }
